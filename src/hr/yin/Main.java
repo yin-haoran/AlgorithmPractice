fe61fa677f2eef46d72d41e43f7a5adf0d72dc6e
yinhaoran <946426726>
2020-11-01 12:43:51 +0800

    二进制中1的个数

--- src/hr/yin/Main.java
+++ src/hr/yin/Main.java
@@ -511,4 +511,19 @@ public class Main {
         return (int) result;
     }
 
+    /**
+     * 二进制中1的个数
+     */
+    public int hammingWeight(int n) {
+        int count = 0;
+
+        while (n != 0) {
+            count++;
+            // 最低位1置0
+            n = n & (n - 1);
+        }
+
+        return count;
+    }
+
 }
